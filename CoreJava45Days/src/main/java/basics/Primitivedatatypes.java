823365c0d7505c259e4b2e78050b6a0d1743c132
Harshini <Harshini@hp>
2026-07-28 19:17:51 +0530

    Basics4-PrimitiveDatatypes

---
+++ CoreJava45Days/src/main/java/basics/Primitivedatatypes.java
@@ -0,0 +1,29 @@
+package basics;
+
+/*  Program :Primitive Data types
+ *  Objective: Printing the primitive data types 
+ */
+public class Primitivedatatypes {
+
+	public static void main(String[] args) {
+	byte b=125;
+	short s=2500; 
+	int i=300;  
+	long l=910076892;//Stores long int values 
+	float f=125.6f;
+	double d=165900043.5671d;
+	char c='c';
+	boolean flag=true;
+	System.out.println("******** Primitive Datatypes **********");
+System.out.println("byte :"+b);
+System.out.println("short :"+s);
+System.out.println("int :"+i);
+System.out.println("long :"+l);
+System.out.println("float :"+f);
+System.out.println("double :"+d);
+System.out.println("char    :"+c);
+System.out.println("boolean :"+flag);
+System.out.println("Primitive datatypes are building blocks of Java ");
+	}
+
+}
