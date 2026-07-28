25e6e1c543f54a45df04b9cc6421c2da3867fa29
Harshini <Harshini@hp>
2026-07-28 18:28:19 +0530

    NBasics2- PersonalInformationPrintingProgram

---
+++ CoreJava45Days/src/main/java/basics/PersonalInformation.java
@@ -0,0 +1,16 @@
+package basics;
+
+public class PersonalInformation {
+
+	public static void main(String[] args) {
+	String name="Harshini";
+	int age=22;
+	String address="ABC";
+	String degree="B.Tech";
+	System.out.println("Name :" +name);
+	System.out.println("Age:"+age);
+	System.out.println("Address :"+address);
+	System.out.println("Degree :"+degree);
+	}
+
+}
