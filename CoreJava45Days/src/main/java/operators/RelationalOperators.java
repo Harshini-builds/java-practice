f16b673502a04f0aca4d84049e90f3ea02357aad
Harshini <Harshini@hp>
2026-07-29 20:02:11 +0530

    Operators4-RelationalOperators

---
+++ CoreJava45Days/src/main/java/operators/RelationalOperators.java
@@ -0,0 +1,20 @@
+package operators;
+
+/* Relational Operators used to compare two values always returns true or false 
+ * 
+ */
+public class RelationalOperators {
+
+	public static void main(String[] args) {
+	int n1=10;
+	int n2=20;
+	System.out.println("Less than operator :"+(n1<n2));
+	System.out.println("Greater than operator :"+(n1>n2));
+	System.out.println("Less than or equals to operator :"+(n1<=n2));
+	System.out.println("Greater than or equals to operator :"+(n1>=n2));
+	System.out.println("Equals operator :"+(n1==n2));
+	System.out.println("Not Equals operator : "+(n1!=n2));
+	
+	}
+
+}
