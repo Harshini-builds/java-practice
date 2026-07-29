9cab83a9a30761e755a91be570dece171547e37d
Harshini <Harshini@hp>
2026-07-29 19:24:08 +0530

    Operators1-ArthimeticOperators

---
+++ CoreJava45Days/src/main/java/operators/ArthimeticOperators.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class ArthimeticOperators {
+public void arthimeticOperations() {
+	int a=50;
+	int b=20;
+	System.out.println("===============================");
+	System.out.println("Arthimetic Operations ");
+	System.out.println("===============================");
+	System.out.println("Addition :"+(a+b));
+	System.out.println("Subtraction :"+(a-b));
+	System.out.println("Multiplication :"+(a*b));
+	System.out.println("Division :"+(a/b));
+	System.out.println("Modulus Division :"+(a%b));
+}
+	public static void main(String[] args) {
+		ArthimeticOperators ap=new ArthimeticOperators();
+		ap.arthimeticOperations();
+
+	}
+
+}
