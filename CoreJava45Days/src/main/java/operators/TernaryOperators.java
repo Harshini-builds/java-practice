2eef360a28559944dcc433141927ea75e7e59e6d
Harshini <Harshini@hp>
2026-07-29 20:22:42 +0530

    Operators6-TernaryOperator

---
+++ CoreJava45Days/src/main/java/operators/TernaryOperators.java
@@ -0,0 +1,20 @@
+package operators;
+
+/* Ternary operator is short form of if-else statement
+ * 
+ */
+
+public class TernaryOperators {
+
+	public static void main(String[] args) {
+	 int a=10;
+	 int b=20;
+	 int largest=(a<b)?a:b;//(condition)? expression1:expression2
+	 System.out.println("====================================");
+	 System.out.println("Ternary Or Conditional Operator");
+	 System.out.println("====================================");
+	 System.out.println("Largest number :"+largest);
+
+	}
+
+}
