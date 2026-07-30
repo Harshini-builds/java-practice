0d3ab0d5864219798a23808330b9703833fea0bc
Harshini-builds <harshinionteddu2728@gmail.com>
2026-07-30 19:07:04 +0530

    conditionalstatements1-EvenOrOdd

---
+++ CoreJava45Days/src/main/java/conditionalstatements/EvenorOdd.java
@@ -0,0 +1,27 @@
+package conditionalstatements;
+
+/*Program : Even or Odd 
+ * Objective: Write a Java program to check whether a given number is Even or Odd.
+ */
+import java.util.Scanner;
+public class EvenorOdd {
+ Scanner scan=new Scanner(System.in);
+	public void checkEvenOrOdd() {
+		System.out.println("Enter a number :");
+		int number=Integer.parseInt(scan.nextLine());
+		System.out.println("===============================");
+		System.out.println("Even or Odd");
+		System.out.println("===============================");
+		if(number%2==0)                                           //validation
+			System.out.println(+number +" is an even number");
+		
+		else 
+			System.out.println(+number + " is an odd number");	
+	}
+	public static void main(String[] args) {
+		EvenorOdd evenorodd=new EvenorOdd();
+		evenorodd.checkEvenOrOdd();
+
+	}
+
+}
