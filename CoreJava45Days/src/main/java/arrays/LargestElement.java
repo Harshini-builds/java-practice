01ea260508123fd3fc9a5c3a2efede4a1a98d6ff
Harshini-builds <harshinionteddu2728@gmail.com>
2026-09-25 19:28:19 +0530

    Arrays 03- To find the largest element of array

---
+++ CoreJava45Days/src/main/java/arrays/LargestElement.java
@@ -0,0 +1,27 @@
+package arrays;
+
+/*
+ * Program:To print the largest element of an array
+ */
+import java.util.Scanner;
+public class LargestElement {
+
+	public static void main(String[] args) {
+	Scanner scan=new Scanner(System.in);
+    System.out.println("Enter length of array ");
+    int arr_length=Integer.parseInt(scan.nextLine());
+    int arr[]=new int[arr_length];
+    System.out.println("Enter elements ");
+    for(int i=0;i<arr_length;i++) {
+    	arr[i]=Integer.parseInt(scan.nextLine());
+    }
+    int largest_element=arr[0];
+    for(int i=0;i<arr_length;i++) {
+    	if(arr[i]>largest_element) {
+    		largest_element=arr[i];
+    	}
+    }
+    System.out.println("Largest element of array is :"+largest_element);
+	}
+
+}
